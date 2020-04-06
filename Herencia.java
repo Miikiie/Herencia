46c5b01aedf0105f0966c1b420a87aee317fcd7b
Miikiie <63179810+Miikiie@users.noreply.github.com>
2020-04-05 23:48:00 -0500

    Create Herencia.java

---
+++ Herencia.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author OHMASTER
+ */
+public class Herencia {
+
+    public static void main(String[] args) {
+        
+        Dog perro1 = new Dog();
+        perro1.LlenarDatosPerro();
+        perro1.VerSalida();
+        
+        Dog perro2 = new Dog();
+        perro2.LlenarDatosPerro();
+        perro2.VerSalida();
+       
+    }
+}
+
+
